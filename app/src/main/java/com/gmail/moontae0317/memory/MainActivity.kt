eb02cd94d23a42f1c1ca3b28f91a344a48f3cd04
MoonTaeYoung <ansxodud238@gmail.com>
2020-05-26 13:28:14 +0900

    20200526 Memory Database(room) - 진행중

---
+++ app/src/main/java/com/gmail/moontae0317/memory/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gmail.moontae0317.memory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
