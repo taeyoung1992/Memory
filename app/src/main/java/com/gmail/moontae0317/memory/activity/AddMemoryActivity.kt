865b9c60c06555b4b9681b72076f261c255103cd
MoonTaeYoung <ansxodud238@gmail.com>
2020-06-01 19:53:37 +0900

    20200601 Memory 1. Database(room) - 데이터 삽입, 수정, 삭제 test 완료
    #######MemoryDao.kt에 비동기 처리 수정해야함########
    
    2. UI구성 - 진행중
    Splash Screen - 완료(레이아웃 구성해야함 - 이미지 필요)
    User - 레이아웃 구성중
    
    3. 기능구현
    camera - 완료
    앨범접근 - 진행

--- app/src/main/java/com/gmail/moontae0317/memory/activity/AddMemoryActivity.kt
+++ app/src/main/java/com/gmail/moontae0317/memory/activity/AddMemoryActivity.kt
@@ -1,26 +1,144 @@
 package com.gmail.moontae0317.memory.activity
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Camera
+import android.graphics.ImageDecoder
+import android.hardware.Camera.open
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.gmail.moontae0317.memory.R
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import kotlinx.android.synthetic.main.activity_add_memory.*
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddMemoryActivity : AppCompatActivity(),View.OnClickListener {
+
+    val REQUEST_IMAGE_CAPTURE = 1  //카메라 사진 촬영 요청 코드 *임의로 값 입력
+    lateinit var currentPhotoPath : String //문자열 형태의 사진 경로값 (초기값을 null로 시작하고 싶을 때 - lateinti var)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_memory)
 
+        setPermission()
+
+    }
+
+    //테드 퍼미션 설정 (카메라 사용시 권한 설정 팝업을 쉽게 구현하기 위해 사용)
+    private fun setPermission() {
+        val permission = object : PermissionListener{
+            override fun onPermissionGranted() {//설정해 놓은 위험권한(카메라 접근 등)이 허용된 경우 이곳을 실행
+                Toast.makeText(this@AddMemoryActivity,"요청하신 권한이 허용되었습니다.",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {//설정해 놓은 위험권한이 거부된 경우 이곳을 실행
+                Toast.makeText(this@AddMemoryActivity,"요청하신 권한이 거부되었습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        TedPermission.with(this)
+            .setPermissionListener(permission)
+            .setRationaleMessage("카메라 앱을 사용하시려면 권한을 허용해주세요.")
+            .setDeniedMessage("권한을 거부하셨습니다.앱을 사용하시려면 [앱 설정]-[권한] 항목에서 권한을 허용해주세요.")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+            .check()
     }
 
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.btn_camera->{
-
+                takeCapture()
             }
             R.id.btn_photo->{
 
             }
         }
+    }
+    //기본 카메라 앱을 사용해서 사진 촬영
+    private fun takeCapture() {
+        //기본 카메라 앱 실행
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile : File? = try{
+                    createImageFile()
+                }catch (e:Exception){
+                    null
+                }
+                photoFile?.also {
+                    val photoURI : Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.gmail.moontae0317.memory.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+    //이미지 파일 생성
+    private fun createImageFile(): File {
+        val timestamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir : File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timestamp}_",".jpg",storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    //startActivityForResult를 통해서 기본 카메라 앱으로 부터 받아온 결과값
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            val bitmap : Bitmap
+            val file = File(currentPhotoPath)
+            if(Build.VERSION.SDK_INT < 28){//안드로이드 9.0 보다 버전이 낮을 경우
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver,Uri.fromFile(file))
+                img_photo.setImageBitmap(bitmap)
+            }else{//안드로이드 9.0 보다 버전이 높을 경우
+                val decode = ImageDecoder.createSource(
+                    this.contentResolver,
+                    Uri.fromFile(file)
+                )
+                bitmap = ImageDecoder.decodeBitmap(decode)
+                img_photo.setImageBitmap(bitmap)
+            }
+            savePhoto(bitmap)
+        }
+    }
+
+    //갤러리에 저장
+    private fun savePhoto(bitmap: Bitmap) {
+        //사진 폴더에 저장하기 위한 경로 선언
+        val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/Pictures/"
+        val timestamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val fileName = "${timestamp}.jpeg"
+        val folder = File(folderPath)
+        if(!folder.isDirectory){//해당 경로에 폴더가 존재하지
+            folder.mkdir() // make directory의 줄임말로 해당경로에 폴더 자동으로
+        }
+        //실제적인 저장 처리
+        val out = FileOutputStream(folderPath + fileName)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+        Log.d("55555555","OK")
+        Toast.makeText(this,"사진이 앨범에 저장되었습니다.",Toast.LENGTH_SHORT).show()
+        Log.d("66666666","OK")
     }
 }
